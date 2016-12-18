fdfdd3c10f02f3ed9539f9c1e3fcb1051a963ba5
Prince <wangyanjie1200@163.com>
2016-12-18 11:06:34 +0800

    init commit

---
+++ etc/dcm_config.py
@@ -0,0 +1,23 @@
+class config(object):
+    def __init__(self):
+        self.server = '10.100.61.3'
+	self.username = None
+        self.password = None
+        self.host_ip = None
+        self.host_os = 'Linux'
+        self.mac = None
+	self.host_name = 'prince'
+
+	self.primary_dns = '10.10.10.10'
+	self.dhcp_server = '0.0.0.0'
+	self.bind_ip = '0.0.0.0'
+cfg = config()
+
+#####
+
+#####
+cfg.username = 'linhai2113'
+cfg.password = 'www4399com'
+cfg.host_ip = '49.140.92.16'
+cfg.mac = 0x50AF7358B457
+cfg.log_path = '/extdisks/sda4/drcom_log' 
